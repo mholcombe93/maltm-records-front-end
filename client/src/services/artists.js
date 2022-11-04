6033e61fc7234ae411da9207bfe7375fb9f5a88a
Matthew Holcombe <matthewholcombe@Matthews-MacBook-Pro-8.local>
2022-11-04 13:52:07 -0400

    basic folder structure

---
+++ client/src/services/artists.js
@@ -0,0 +1,44 @@
+// export const getStudents = async () => {
+//   try {
+//     const response = await api.get("/students");
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// export const getStudent = async (id) => {
+//   try {
+//     const response = await api.get(`/students/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// export const createStudent = async (studentData) => {
+//   try {
+//     const response = await api.post("/students", studentData);
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// export const updateStudent = async (id, studentData) => {
+//   try {
+//     const response = await api.put(`/students/${id}`, studentData);
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// export const deleteStudent = async (id) => {
+//   try {
+//     const response = await api.delete(`/students/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
