2840031f32a79a3d88aa36a54e02692fd634177f
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-11-06 08:44:33 -0500

    create a mockup nav bar and possible re-brand to present to group

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,20 @@
+import "./Nav.css"
 import { NavLink } from "react-router-dom"
+import cassette from "../images/cassette-tape-no-title.png"
 
 function Nav() {
   return (
-    <div className="nav">
-      <NavLink to="/">Home/Logo</NavLink>
-      <NavLink to="/artists">Artists</NavLink>
-      <NavLink to="/albums">Albums</NavLink>
-      <NavLink to="/">Genres</NavLink>
-      <NavLink to="/songs">Songs</NavLink>
-    </div>
+    <>
+       <h1>B-Side Music</h1>
+      <div className="nav">
+      <NavLink to="/"><img src={cassette} alt="cassette logo" className="cassette-logo"/></NavLink>
+       <div className="navlink"> <NavLink to="/artists">Artists</NavLink></div>
+      <div className="navlink"><NavLink to="/albums">Albums</NavLink></div>
+      <div className="navlink"><NavLink to="/genres">Genres</NavLink></div>
+      <div className="navlink"><NavLink to="/songs">Songs</NavLink></div>
+      </div>
+ </>
+    
   )
 }
 
