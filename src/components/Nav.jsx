65cf9208a9873fa51b00af9190311194ccaf0cc1
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-11-06 12:01:21 -0500

    add media query to nav bar. selected background color for app.js

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,16 +4,17 @@ import cassette from "../images/cassette-tape-no-title.png"
 
 function Nav() {
   return (
-    <>
-       <h1>B-Side Music</h1>
-      <div className="nav">
-      <NavLink to="/"><img src={cassette} alt="cassette logo" className="cassette-logo"/></NavLink>
-       <div className="navlink"> <NavLink to="/artists">Artists</NavLink></div>
-      <div className="navlink"><NavLink to="/albums">Albums</NavLink></div>
-      <div className="navlink"><NavLink to="/genres">Genres</NavLink></div>
-      <div className="navlink"><NavLink to="/songs">Songs</NavLink></div>
+    <div className="nav-bar-component">
+      <div className="title-logo">
+      <NavLink to="/"><img src={cassette} alt="cassette logo" className="cassette-logo" /></NavLink><h1>B-Side Music</h1>
       </div>
- </>
+      <div className="nav">
+          <div className="navlink"> <NavLink to="/artists">Artists</NavLink></div>
+          <div className="navlink"><NavLink to="/albums">Albums</NavLink></div>
+          <div className="navlink"><NavLink to="/genres">Genres</NavLink></div>
+          <div className="navlink"><NavLink to="/songs">Songs</NavLink></div>
+      </div>
+ </div>
     
   )
 }
