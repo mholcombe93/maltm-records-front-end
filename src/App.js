0ff647ccc4af73083a44e40eef8f3a170f7a0f20
Matthew Holcombe <matthewholcombe@Matthews-MacBook-Pro-8.local>
2022-11-07 11:26:17 -0500

    base api works

--- src/App.js
+++ src/App.js
@@ -5,10 +5,27 @@ import ArtistSearch from "./screens/ArtistSearch.jsx";
 import Albums from "./screens/Albums";
 import About from "./screens/About.jsx";
 import Genres from "./screens/Genres";
+import axios from "axios"
+import React, { useState, useEffect } from "react"
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  
+  
+  const URL = "https://b-side-music.up.railway.app/"
+  const [call, setCall] = useState([])
+  useEffect(() => {
+   const callApi =  async () => {
+      const response = await axios(URL)
+      setCall(response.data)
+      console.log(response.data)
+  }
+     callApi()  
+      
+  }, [])
+
+
   return (
     <>
       <Nav />
