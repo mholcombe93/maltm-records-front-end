b839d198d5c62eef04858f72d10c5d9d2afff3ce
Tashon Forrester <tashonvforrester@gmail.com>
2022-11-08 11:19:35 -0500

    started working on filter page

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -3,10 +3,13 @@ import { artists } from "../services/artists"
 
 const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
 
-function filter() {
+function filter({artists}) {
+  let alphabet = {};
+  
+
 
   return (
-    <button onClick={alphabet[0]}></button>
+   console.log(filter)
   )
 }
 
