592890f0d88af3d65c908b082c810c0755fea0c9
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-08 17:13:52 -0500

    finalized CRUD, debugged with Raul

--- src/screens/ArtistDetail.jsx
+++ src/screens/ArtistDetail.jsx
@@ -2,25 +2,25 @@ import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getArtist } from "../services/artists.js";
 
+
+
 function ArtistDetail() {
   const [artist, setArtist] = useState({});
-  const [artistName, setArtistName] = useState("")
-
   const { artistID } = useParams();
 
   useEffect(() => {
     const fetchArtist = async () => {
       const response = await getArtist(artistID);
       setArtist(response);
-      const res = await getArtist(artistName)
-      setArtistName(res)
     };
     fetchArtist();
   }, []);
 
   return (
     <div>
-      <h1>{artist.artistName}</h1>
+      <h1>Add Album</h1>
+      <h2>{artist.name}</h2>
+      
 
       <Link to={`/artists/${artist._id}/add-album`}>
         <button className="addAlbumButton">add album</button>
