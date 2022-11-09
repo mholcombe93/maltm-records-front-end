30a0aece9c3352f1e84bad274f4879c1660a9b4e
Tashon Forrester <tashonvforrester@gmail.com>
2022-11-09 13:13:57 -0500

    search filter component worked on with Chris

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -8,32 +8,17 @@ const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M
 
 
 function SearchFilter({ albums }) {
-  const [character,setCharacter]=useState()
+  const [character,setCharacter]=useState("")
 
   
 
   return (
-    
-    <FilterCharacter character={alphabet[0]} />
-    
+        
+      <FilterCharacter character={alphabet[0]} />
+      
   )
 }
 
 export default SearchFilter
-=======
-import { artists } from "../services/artists"
 
-const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
-
-function filter({artists}) {
-  let alphabet = {};
-  
-
-
-  return (
-   console.log(filter)
-  )
-}
-
-export default filter
 
