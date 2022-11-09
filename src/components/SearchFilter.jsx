a6de81020a64ad1681425bfcb4927cc85ece2567
Malik Washington <malikwashington@gmail.com>
2022-11-08 21:46:35 -0500

    was working with Tashon on SearchFilter hit a block

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -1,24 +1,77 @@
 import React, { useState } from "react";
-import { albums } from "../services/albums"
-import FilterCharacter from "./FilterCharacter"
-
-
-const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","#"]
-
-
+import { Link } from "react-router-dom";
+import { albums } from "../services/albums";
+import FilterCharacter from "./FilterCharacter";
+import AlbumCover from "./AlbumCover";
 
 function SearchFilter({ albums }) {
-  const [character,setCharacter]=useState()
+  const [character, setCharacter] = useState();
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "#",
+  ];
+  let arr = [];
+  const handleClick = (e, letter) => {
+    e.preventDefault();
+    setCharacter(letter)
+    // albums.filter((album) => {
+    //   if (
+    //     album.title.split("")[0].toLowerCase().includes(letter.toLowerCase())
+    //   ) {
+    //     arr.push(album);
 
-  
+    //     arr.map((album) => <AlbumCover key={album._id} album={album} />);
+    //   }
+    // });
+  };
 
-  return (
-    
-    <FilterCharacter character={alphabet[0]} />
-    
-  )
+  return alphabet.map((letter) => {
+    return (
+      <div className="alphabet" onClick={(e) => handleClick(e, letter)}>
+        {letter}
+
+        <div className="gallery">
+          {albums.filter((album) => {
+            if (
+              album.title
+                .split("")[0]
+                .toUpperCase()
+                .includes(character)
+            ) {
+              // arr.push(album);
+              console.log(album)
+              arr.map((album) => (<AlbumCover key={album._id} album={album} />))
+            }
+          })}
+        </div>
+      </div>
+    );
+  });
 }
 
-export default SearchFilter
-
-
+export default SearchFilter;
--- src/screens/AlbumSearch.jsx
+++ src/screens/AlbumSearch.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import AlbumCover from "../components/AlbumCover.jsx";
 import Searchbar from "../components/Searchbar";
 import "../App.css"
+import SearchFilter from "../components/SearchFilter.jsx";
 
 function AlbumSearch ({ albums }) {
   const [searchTerm, setSearchTerm] = useState("");
@@ -10,6 +11,10 @@ function AlbumSearch ({ albums }) {
     <>
       <div>
         <h1>Albums</h1>
+        <div className="alpha">
+        <SearchFilter albums={albums} />
+        </div>
+        <br></br>
         <Searchbar setSearchTerm={setSearchTerm} />
         <div className="gallery">
           {albums
