fb463b67265c273f557525d725c1a650a6c4f1f8
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-09 13:44:54 -0500

    modify artist imagepage

--- src/components/ArtistImage.jsx
+++ src/components/ArtistImage.jsx
@@ -3,7 +3,8 @@ function ArtistImage({artist}) {
   return (
     <>
       <div className="image">
-      <img src={artist.artistImg} className="gallery-pic" alt="Artist Image" />
+        <img src={artist.artistImg} className="gallery-pic" alt="Artist Image" />
+        <p>{artist.name}</p>
       </div>
     </>
   )
